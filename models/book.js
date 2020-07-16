ae77d1838824e00cdfa7cf0a61a80c4c07ff35ab
Kervcodes-desktop <Kervintz84@hotmail.com>
2020-07-15 21:04:17 -0400

    Update book.js

--- models/book.js
+++ models/book.js
@@ -7,10 +7,36 @@ module.exports = (sequelize) => {
   class Book extends Sequelize.Model {}
   Book.init(
     {
-      title: Sequelize.STRING,
-      author: Sequelize.STRING,
-      genre: Sequelize.STRING,
-      year: Sequelize.INTEGER,
+      title: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        validate: {
+          notNull: {
+            msg: 'Please provide a value for "title"',
+          },
+          notEmpty: {
+            msg: 'Please provide a value for "title"',
+          },
+        },
+      },
+      author: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        validate: {
+          notNull: {
+            msg: 'Please provide a value for "title"',
+          },
+          notEmpty: {
+            msg: 'Please provide a value for "title"',
+          },
+        },
+      },
+      genre: {
+        ype: Sequelize.STRING
+      },
+      year: {
+        ype: Sequelize.INTEGER
+      },
     },
     { sequelize }
   );
